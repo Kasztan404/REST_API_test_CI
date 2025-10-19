3be65975890d04b10192ec7e09dd545daa935cac
root <m.pestka@protonmail.com>
2025-10-19 18:40:58 +0200

    Adds API server as AUT

---
+++ routers/todos.js
@@ -0,0 +1,15 @@
+import express from 'express'
+
+const router = express.Router();
+
+let todos = [
+    { id: 1, description: "Naucz się expressJS", done: false },
+    { id: 2, descritpion: "Naucz się PactumJS", done: false }
+]
+
+
+router.get('/', (req, res) => {
+    res.json(todos);
+});
+
+export const routerTodos = router;
